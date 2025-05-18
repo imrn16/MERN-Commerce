9b4afe697ef85d669bcf1ebeb344fbe935b19dd8
imrn16 <151978120+imrn16@users.noreply.github.com>
2025-05-17 19:48:04 -0500

    modified:   frontend/src/pages/Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,53 +15,28 @@ const Home = () => {
 	return (
 		<>
 			{!keyword ? <Header /> : null}
-			{isLoading ? (
-				<Loader />
-			) : isError ? (
-				<Message variant="danger">{isError?.data?.message || isError.error}</Message>
-			) : (
-				<>
-					
-
-					{/* <div className="relative flex bg-red-900 h-[40rem] w-[40rem]">
+			{
+				// isLoading ? (
+				// 	<Loader />
+				// ) :
+				isError ? (
+					<Message variant="danger">{isError?.data?.message || isError.error}</Message>
+				) : (
+					<>
+						{/* <div className="relative flex bg-red-900 h-[40rem] w-[40rem]">
 						<div className="flex bg-green-900 h-[20rem] w-[20rem]"></div>
 						<div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-900 h-[20rem] w-[20rem] flex justify-center items-center">
 							<div>test</div>
 						</div>
 					</div> */}
 
-					<div className="overflow-x-hidden flex flex-col items-center justify-center w-full bg-rede-900 mt-[10rem]">
-						<h1
-							style={{
-								fontSize: `min(7vw, 7vh)`,
-							}}
-							className="flex text-[2rem]d font-semibold">
-							Best In The Business
-						</h1>
-
-						<h3
-							style={{
-								fontSize: `min(3vw, 3vh)`,
-							}}
-							className="text-stone-500 mb-10">
-							The biggest brands in the world, all in one place
-						</h3>
-					</div>
-
-					<div>
-						<SliderCarousel />
-					</div>
-					<div className="bg-green-900s relative">
-					<div  className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-600 h-[50%] w-[50%] flex justify-center items-center blur-[200px] md:blur-[400px] rounded-full opacity-70">
-							<div>test</div>
-						</div>
-						<div className="flex flex-col items-center justify-center w-full bg-reds-900 mt-[10rem]">
+						<div className="overflow-x-hidden flex flex-col items-center justify-center w-full bg-rede-900 mt-[10rem]">
 							<h1
 								style={{
 									fontSize: `min(7vw, 7vh)`,
 								}}
 								className="flex text-[2rem]d font-semibold">
-								One Stop Shop
+								Best In The Business
 							</h1>
 
 							<h3
@@ -69,30 +44,55 @@ const Home = () => {
 									fontSize: `min(3vw, 3vh)`,
 								}}
 								className="text-stone-500 mb-10">
-								Everything you need to capture the moment
+								The biggest brands in the world, all in one place
 							</h3>
 						</div>
 
-						<div className="md:flex w-full bg-reds-900 max-w-full">
-							<div className="flex w-full md:w-1/2 bg-greens-900 md:pr-10">
-								<div className="w-full">
-									<FadeCarousel
-										label={`Cameras`}
-										category={`673a6e2b811e7585f48c2e86`}
-									/>
-									<FadeCarousel label={`Accessories`} />
-								</div>
+						<div>
+							<SliderCarousel />
+						</div>
+						<div className="bg-green-900s relative">
+							<div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-600 h-[50%] w-[50%] flex justify-center items-center blur-[200px] md:blur-[400px] rounded-full opacity-70">
+								<div>test</div>
+							</div>
+							<div className="flex flex-col items-center justify-center w-full bg-reds-900 mt-[10rem]">
+								<h1
+									style={{
+										fontSize: `min(7vw, 7vh)`,
+									}}
+									className="flex text-[2rem]d font-semibold">
+									One Stop Shop
+								</h1>
+
+								<h3
+									style={{
+										fontSize: `min(3vw, 3vh)`,
+									}}
+									className="text-stone-500 mb-10">
+									Everything you need to capture the moment
+								</h3>
 							</div>
 
-							<div className="flex md:w-1/2 bg-blues-900 md:pl-10 md:items-center md:justify-center">
-								<div className="w-full">
-									<FadeCarousel label={`Lenses`} />
+							<div className="md:flex w-full bg-reds-900 max-w-full">
+								<div className="flex w-full md:w-1/2 bg-greens-900 md:pr-10">
+									<div className="w-full">
+										<FadeCarousel
+											label={`Cameras`}
+											category={`673a6e2b811e7585f48c2e86`}
+										/>
+										<FadeCarousel label={`Accessories`} />
+									</div>
+								</div>
+
+								<div className="flex md:w-1/2 bg-blues-900 md:pl-10 md:items-center md:justify-center">
+									<div className="w-full">
+										<FadeCarousel label={`Lenses`} />
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					{/* <div>
+						{/* <div>
 						<div
 							className="grid 
             
@@ -114,7 +114,7 @@ const Home = () => {
 						</div>
 					</div> */}
 
-					{/* <div className="flex flex-wrap gap-4 justify-center">
+						{/* <div className="flex flex-wrap gap-4 justify-center">
 						<div className="flex-1 min-w-[14rem] max-w-[20rem] h-48 bg-blue-300">Component 1</div>
 						<div className="flex-1 min-w-[14rem] max-w-[20rem] h-48 bg-green-300">Component 2</div>
 						<div className="flex-1 min-w-[14rem] max-w-[20rem] h-48 bg-red-300">Component 3</div>
@@ -133,7 +133,7 @@ const Home = () => {
 						<div className="flex-1 min-w-[14rem] max-w-[20rem] h-48 bg-yellow-300">Component 16</div>
 					</div> */}
 
-					{/* <div className="grid grid-cols-[repeat(auto-fill,_minmax(12rem,_1fr))] gap-2 justify-center">
+						{/* <div className="grid grid-cols-[repeat(auto-fill,_minmax(12rem,_1fr))] gap-2 justify-center">
   <div className="aspect-[10/12] bg-blue-300">Component 1</div>
   <div className="aspect-[10/12] bg-green-300">Component 2</div>
   <div className="aspect-[10/12] bg-red-300">Component 3</div>
@@ -151,8 +151,9 @@ const Home = () => {
   <div className="aspect-[10/12] bg-red-300">Component 15</div>
   <div className="aspect-[10/12] bg-yellow-300">Component 16</div>
 </div> */}
-				</>
-			)}
+					</>
+				)
+			}
 		</>
 	);
 };
